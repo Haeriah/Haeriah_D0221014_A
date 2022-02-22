b6226af0d6de0619b729e050a0b63d3b8f3fcc5a
Haeriah_D0221014_A <99958251+Haeriah@users.noreply.github.com>
2022-02-22 16:48:30 +0800

    Add files via upload

---
+++ day4_ java_stdin_and_stdout II .java
@@ -0,0 +1,22 @@
+ package com.day4_java_stdin_and_stdout II java;
+
+ import java.until.Scanner;
+
+
+ public class main {
+
+  public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  int i = scan nextInt();
+  double d = scan nextdouble();
+  scan .nextLine();
+  String s = scan.nextLine();
+  
+ 
+  System.out.println("String:" + s);
+  System.out.println("Double:" + d);
+  System.out.println("Int:" + i);
+ 
+   
+  }
+}
